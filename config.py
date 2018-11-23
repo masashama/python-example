99addf4dede3adbd68085cb4f0888f6c25d88aef
Александр <masashama@gmail.com>
2018-11-23 16:12:05 +0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import re
-
-GOOGLE_SHEET = "https://script.google.com/macros/s/AKfycbxeexe3L_t5OVb_bvAEgb6Bwf98vbLH_7xbdv9qaoFlal7XK4ob/exec"
-TOKEN = "296172584:AAGvSF-uAjR9qPZjqUFskD1AHZQmYsK_6X0"
-TOKEN = "278233013:AAF77OxhW9gePbGG1IYvB504JED-xm-btVQ"
-
-AMOUNT_REGEXP = re.compile(r'^(\d+\.?\d*)', re.IGNORECASE)
-PAYSYTEM_REGEXP = re.compile(r'^\d+\.?\d* (\w+)', re.IGNORECASE)
-COSTS_REGEXP = re.compile(r'^\d+\.?\d* \w+ ([\w /]+)$', re.IGNORECASE)
